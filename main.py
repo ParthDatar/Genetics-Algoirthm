3ac2931e83cbbd09e6bb27e4eb72139417fcc03c
ParthDatar <parth.a.datar@vanderbilt.edu>
2021-11-10 19:29:04 -0600

    Fitness started

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import robby 
-rw = robby.World(10, 10)
 import random as rn
 
 # rw.demo(rw.strategyM)
 
 class organism:
-	def __init__(self):
-		self.genome = [rn.randint(0, 6) for x in xrange(243)]
-		self.score = 0
+	def __init__(self, genes):
+		self.genome = [rn.randint(0, 6) for x in xrange(genes)]
+
+class population:
+	def __init__(self, size, steps, crossover, mutation, generations):
+		self.genomes = [organism(243) for x in xrange(size)]
+		self.size = size
+		self.steps = steps
+		self.mutation = mutation
+		self.crossover = crossover
+		self.generations = generations
+
+
+	def fitness(genome):
+		rw = robby.World(10, 10)
+		for i in xrange(steps):
+			p = rw.getPerceptCode()
+			rw.performAction(robby.POSSIBLE_ACTIONS[genome[p]])
+			
+	
+
+	def sortByFitness(self): 
+		tuples = [(fitness(g), g) for g in genomes] 
+		tuples.sort() 
+		sortedFitnessValues = [f for (f, g) in tuples] 
+		sortedGenomes = [g for (f, g) in tuples] 
+		return sortedGenomes, sortedFitnessValues
 
 
 

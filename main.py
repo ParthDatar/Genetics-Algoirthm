3a558f328cda17f301734cde51bd31f8a286f84a
ParthDatar <parth.a.datar@vanderbilt.edu>
2021-11-10 19:07:15 -0600

    Organism

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import robby 
 rw = robby.World(10, 10)
+import random as rn
 
-rw.demo(rw.strategyM)
+# rw.demo(rw.strategyM)
+
+class organism:
+	def __init__(self):
+		self.genome = [rn.randint(0, 6) for x in range(243)]
+		self.score = 0
+		
+

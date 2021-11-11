c0bda23196d1116cfb0966a2fd28147a8d34f0f8
ParthDatar <parth.a.datar@vanderbilt.edu>
2021-11-10 19:38:24 -0600

    Fitness working

--- main.py
+++ main.py
@@ -6,31 +6,33 @@ import random as rn
 class organism:
 	def __init__(self, genes):
 		self.genome = [rn.randint(0, 6) for x in xrange(genes)]
+		self.fit = 0
+	def fitness(self):
+		fs = [0 for x in xrange(100)]
+		for i in xrange(100):
+			rw = robby.World(10, 10)
+			for j in xrange(steps):
+				perc = rw.getPerceptCode()
+				reward = rw.performAction(robby.POSSIBLE_ACTIONS[self.genome[perc]])
+				fs[i] += reward
+		return sum(fs)/100
+			
 
 class population:
 	def __init__(self, size, steps, crossover, mutation, generations):
-		self.genomes = [organism(243) for x in xrange(size)]
+		self.orgs = [organism(243) for x in xrange(size)]
 		self.size = size
 		self.steps = steps
 		self.mutation = mutation
 		self.crossover = crossover
 		self.generations = generations
-
-
-	def fitness(genome):
-		rw = robby.World(10, 10)
-		for i in xrange(steps):
-			p = rw.getPerceptCode()
-			rw.performAction(robby.POSSIBLE_ACTIONS[genome[p]])
-			
 	
-
 	def sortByFitness(self): 
-		tuples = [(fitness(g), g) for g in genomes] 
+		tuples = [(o.fitness(), o) for o in orgs] 
 		tuples.sort() 
-		sortedFitnessValues = [f for (f, g) in tuples] 
-		sortedGenomes = [g for (f, g) in tuples] 
-		return sortedGenomes, sortedFitnessValues
+		sortedFitnessValues = [f for (f, o) in tuples] 
+		sortedOrgs = [o for (f, o) in tuples] 
+		return sortedOrgs, sortedFitnessValues
 
 
 
